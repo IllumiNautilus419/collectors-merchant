baffb6e1da04199777e167a35fa9529ba3227614
IllumiNautilus159 <dciphermarketing@gmail.com>
2024-02-14 11:37:50 +1300

    Begin Class Structure
    Start adding class foundations and establishing namespaces

--- 2024_Staging/wp-content/plugins/2024-CM/classes/core/classCore.php
+++ 2024_Staging/wp-content/plugins/2024-CM/classes/core/classCore.php
@@ -4,9 +4,35 @@ namespace CMERCH;
  
 class CORE{
 
+    protected $user;
+    protected $user_id;
+    public $user_name;
+    protected $user_roles;
+
     public function __construct(){
 
+        if($user = wp_get_current_user()){
+
+            // save og user object
+            $this->user=$user;
+
+            // pull useful values
+            $this->user_values($user);
+
+        }else{
+            $user = null;
+        }
+
     } 
+
+    // get user property values
+    private function user_values($user){
+
+        $this->user_id = $user->ID;
+        $this->user_name = $user->display_name;
+        $this->user_roles = $user->roles;
+
+    }
 
     // Testing function to make sure class has init correctly
     public function ping($message = "Pong!"){
